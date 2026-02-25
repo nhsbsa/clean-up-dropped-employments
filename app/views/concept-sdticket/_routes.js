e674241a22114b4d63c1cfc5234f05fa16beb54a
katie.twinn@nhsbsa.nhs.uk <katie.twinn@nhsbsa.nhs.uk>
2026-02-25 15:39:10 +0000

    updating the sd report concept

--- app/views/concept-sdticket/_routes.js
+++ app/views/concept-sdticket/_routes.js
@@ -4,15 +4,15 @@ const router = express.Router();
 
 
 router.post('/search', function (req, res) {
- 
     res.redirect('results');
-
 })
 
 router.post('/results', function (req, res) {
- 
     res.redirect('report');
+})
 
+router.post('/report', function (req, res) {
+    res.redirect('confirm-report');
 })
 
 
