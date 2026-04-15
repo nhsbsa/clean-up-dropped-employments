5b764881e12b3eff4cdeaaf570efc1a0b9452a03
katie.twinn@nhsbsa.nhs.uk <katie.twinn@nhsbsa.nhs.uk>
2026-04-15 14:52:28 +0100

    creating prototype for issue message 2

--- app/routes.js
+++ app/routes.js
@@ -58,7 +58,7 @@ router.use('/concept-compare', require('./views/concept-compare/_routes'));
 router.use('/concept-sdticket', require('./views/concept-sdticket/_routes'));
 router.use('/concept-all', require('./views/concept-all/_routes'));
 router.use('/issue-1', require('./views/issue-1/_routes'));
-// router.use('/issue-1', require('./views/issue-2/_routes'));
+router.use('/issue-1', require('./views/issue-2/_routes'));
 // router.use('/issue-1', require('./views/issue-3/_routes'));
 router.use('/issue-1', require('./views/issue-4/_routes'));
 // router.use('/issue-1', require('./views/issue-5/_routes'));
