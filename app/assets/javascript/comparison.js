845822641e2b1094a054cdb68ca3023df33855fd
katie.twinn@nhsbsa.nhs.uk <katie.twinn@nhsbsa.nhs.uk>
2026-04-29 13:29:48 +0100

    updating the compare feature for errors

--- app/assets/javascript/comparison.js
+++ app/assets/javascript/comparison.js
@@ -1,14 +1,17 @@
 document.getElementById("compare-form").addEventListener("submit", function (e) {
     const checkboxes = document.querySelectorAll(".compare-checkboxes__input:checked");
     const error = document.getElementById("checkboxError");
+    const errorSummary = document.getElementById("compareErrorSummary");
     const formGroup = document.querySelector(".compareGroup");
 
     if (checkboxes.length !== 2) {
         e.preventDefault(); // stop form submission
         error.style.display = "block"; // show error message
+        errorSummary.style.display = "block"; // show error summary
         formGroup.classList.add("nhsuk-form-group--error"); // highlight form group
     } else {
         error.style.display = "none"; // hide error
+        errorSummary.style.display = "hide"; // hide error summary
         formGroup.classList.remove("nhsuk-form-group--error"); // remove error highlight
     }
 });
